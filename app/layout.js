e36fdca221dcbcfdcb3a49e5c4d9843c814ea204
Salman Dabbakuti <dabbakutisalman@gmail.com>
2024-06-28 15:08:32 +0530

    added: client boilerplate with nextjs, antd, thirdweb, ethers

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import SiteLayout from "./components/SiteLayout";
+import Web3Provider from "./components/Web3Provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Web3Provider>
+          <SiteLayout>{children}</SiteLayout>
+        </Web3Provider>
+      </body>
     </html>
   );
 }
